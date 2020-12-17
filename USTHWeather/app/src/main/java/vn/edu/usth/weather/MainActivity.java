3e5e542c2c9e928f30f9854afd0a253ef4685d57
huynguyenanh24 <huynguyenanh24@gmail.com>
2020-12-17 10:23:53 +0700

    update labwork2

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/MainActivity.java
@@ -0,0 +1,48 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private String TAG ="run";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(TAG,"OnResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(TAG,"OnPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(TAG,"OnStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(TAG,"OnDestroy");
+    }
+}
